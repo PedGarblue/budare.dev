3157c2f7095057ccb39d63829a467b41332af1d7
PedGarblue <pedro.alejandro.g@hotmail.com>
2021-06-07 10:09:21 -0400

    Optimization for font awesome plugin

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,12 +1,59 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+  faArrowUp,
+  faCertificate,
+  faClipboardCheck,
+  faCode,
+  faCube,
+  faEnvelope,
+  faGlobeAmericas,
+  faPhoneSquare,
+  faProjectDiagram,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import {
+  faCss3Alt,
+  faGithub,
+  faHtml5,
+  faJs,
+  faLinkedin,
+  faNodeJs,
+  faPhp,
+  faTelegram,
+  faVuejs,
+} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(fas);
-library.add(far);
-library.add(fab);
+// Solid Icons
+library.add([
+  faArrowUp,
+  faGlobeAmericas,
+  faPhoneSquare,
+  faEnvelope,
+  faCode,
+  faProjectDiagram,
+  faCertificate,
+  faUsers,
+  faCube,
+  faClipboardCheck,
+]);
+
+// Regular Icons
+library.add([faUser]);
+
+// Brands Icons
+library.add([
+  faGithub,
+  faLinkedin,
+  faTelegram,
+  faVuejs,
+  faNodeJs,
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faPhp,
+]);
 
 export default {
   install(app) {
