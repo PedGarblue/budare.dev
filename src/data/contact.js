f87230f10766d9c2c97eec853aaf474a2e7205ca
Pedro García <pedro.alejandro.g@hotmail.com>
2024-07-18 21:03:49 -0400

    feat(content): updated cv and email

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,8 +1,8 @@
 export default {
   linkedin: 'https://www.linkedin.com/in/pedro-garcia-acosta/',
-  email: 'garcia.pedro.a20@gmail.com',
+  email: 'hello@budare.dev',
   telegram: 'https://t.me/pedgarcia',
   github: 'https://github.com/PedGarblue',
   phone: '+58 414 4803943',
-  resume: 'https://drive.google.com/file/d/16SKz8kxkYbnRTTaG17FwAJwpNxkeOZjR/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/1NfVEpk7z0G-iCZBclp5UhuqZqXGSEhAo/view?usp=sharing',
 };
